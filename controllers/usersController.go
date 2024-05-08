38aafd6b1c11cb74271aac1e89d537f78579dfee
Arajdian Altaf <arajdianaltaf123@gmail.com>
2024-05-09 01:10:18 +0700

    Add UserUpdate

--- controllers/usersController.go
+++ controllers/usersController.go
@@ -1,13 +1,12 @@
 package controllers
 
 import (
-	"net/http"
 	"github.com/Arajdian-Altaf/final-task-pbi/models"
 	valid "github.com/asaskevich/govalidator"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
+	"net/http"
 )
-
 
 func UserCreate(c *gin.Context) {
 	DB := c.MustGet("db").(*gorm.DB)
@@ -37,6 +36,41 @@ func UserCreate(c *gin.Context) {
 		})
 		return
 	}
+
+	c.JSON(200, gin.H{
+		"user": user,
+	})
+}
+
+func UserUpdate(c *gin.Context) {
+	DB := c.MustGet("db").(*gorm.DB)
+
+	// Get id from URL
+	id := c.Param("id")
+
+	var user models.User
+	DB.First(&user, id)
+
+	var userBody struct {
+		Username string
+		Email    string `valid:"email"`
+		Password string `valid:"minstringlength(6)"`
+	}
+
+	c.Bind(&userBody)
+
+	if _, err := valid.ValidateStruct(userBody); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	DB.Model(&user).Updates(models.User{
+		Username: userBody.Username,
+		Email: userBody.Email,
+		Password: userBody.Password,
+	})
 
 	c.JSON(200, gin.H{
 		"user": user,
