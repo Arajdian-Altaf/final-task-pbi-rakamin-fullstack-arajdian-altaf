d45bc8a7cc0d62a12be1943578981918c957a7c5
Arajdian Altaf <arajdianaltaf123@gmail.com>
2024-05-08 23:14:27 +0700

    Add user and photo model

---
+++ models/photoModel.go
@@ -0,0 +1,14 @@
+package models
+
+import (
+	"gorm.io/gorm"
+)
+
+type Photo struct {
+	gorm.Model
+	Title    string
+	Caption  string
+	PhotoURL string
+	UserID   uint `gorm:"index"`
+	User     User `gorm:"foreignKey:UserID"`
+}
