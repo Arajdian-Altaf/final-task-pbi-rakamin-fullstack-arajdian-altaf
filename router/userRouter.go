15bee787821629c9c6abb1b791b7b8436116b453
Arajdian Altaf <arajdianaltaf123@gmail.com>
2024-05-10 22:51:26 +0700

    Add jwt middleware and controllers to user routes

--- router/userRouter.go
+++ router/userRouter.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"github.com/Arajdian-Altaf/final-task-pbi/controllers"
+	"github.com/Arajdian-Altaf/final-task-pbi/middlewares"
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,8 +10,8 @@ func UserRoutes(route *gin.Engine) {
 	user := route.Group("/users") 
 	{
 		user.POST("/register", controllers.UserCreate)
-		user.GET("/login", func(c *gin.Context) {})
-		user.PUT("/:userId", func(c *gin.Context) {})
-		user.DELETE("/:userId", func(c *gin.Context) {})
+		user.GET("/login", controllers.UserLogin)
+		user.PUT("/:userId", middlewares.JWTMiddleware(), controllers.UserUpdate)
+		user.DELETE("/:userId", middlewares.JWTMiddleware(), controllers.UserDelete)
 	}
 }
